86386af8540064c600789b2daa2e48f6ea6ed142
Kyle Jenne <strongagent1@gmail.com>
2023-01-19 09:51:31 -0500

    completed LINQ exercise - KJ

---
+++ LINQ-Exercise/LINQ/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LINQ;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+    {
+        static void Main(string[] args)
+        {
+        var videoGameList = new List<string>() { "MegaMan", "Call Of Duty", "Contra", "Tetris", "MineCraft", "Street Fighter" };
+
+        var orderedList = videoGameList.OrderBy(x => x.Length).ToList();
+        foreach(var item in orderedList)
+        {
+            Console.WriteLine(item);
+        }
+
+
+        }
+    }
+
